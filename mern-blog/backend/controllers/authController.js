5ad24b738b148b5ee01ca82c44d6530577944b8e
anistlh-star <anistlh@gmail.com>
2025-11-06 07:29:40 +0000

    Update isAdmin property in authController
    Change isAdmin property to return 'Not admin' if false

--- mern-blog/backend/controllers/authController.js
+++ mern-blog/backend/controllers/authController.js
@@ -49,7 +49,6 @@ export const login = async (req, res) => {
         id: user._id,
         name: user.name, // ← Make sure this is included
         email: user.email,
-        isAdmin: user.isAdmin || "Not admin",
       },
       process.env.JWT_SECRET,
       {
